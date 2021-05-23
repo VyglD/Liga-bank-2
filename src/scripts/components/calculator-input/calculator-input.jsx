525d430360aa4dc5ec2465bd7cc5ac4a9f949393
VyglD <vygld@yandex.ru>
2021-05-23 08:25:17 +0300

    calc-input

---
+++ src/scripts/components/calculator-input/calculator-input.jsx
@@ -0,0 +1,255 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {Key} from "../../constants";
+
+const REGEX_DIGITS = /^\d+$/;
+
+const WRAPPER_CLASS = `calculator-params__input-wrapper`;
+
+const CustomClass = {
+  WRAPPER: WRAPPER_CLASS,
+  WRAPPER_INVALID: `${WRAPPER_CLASS}--invalid`,
+};
+
+const getDefaultStepValue = (min, max) => {
+  return parseInt((max - min) / 10, 10);
+};
+
+const getCleanDigit = (dirtyValue) => {
+  if (typeof dirtyValue === `number`) {
+    return dirtyValue;
+  }
+
+  const cleanDigit = parseInt(
+      String(dirtyValue).split(``).filter((char) => REGEX_DIGITS.test(char)).join(``),
+      10
+  );
+
+  if (isNaN(cleanDigit)) {
+    return 0;
+  }
+
+  return cleanDigit;
+};
+
+const getFormatedValue = (value) => {
+  if (typeof value === `string`) {
+    value = getCleanDigit(value);
+  }
+
+  return value.toLocaleString();
+};
+
+const CalculatorInput = (props) => {
+  const {
+    labelClassName,
+    labelText,
+    inputClassName,
+    inputId,
+    minValue,
+    maxValue,
+    stepValue = getDefaultStepValue(minValue, maxValue),
+    currentValue,
+    onCurrentValueChange,
+    postfix,
+    controls,
+    // range,
+    // movingRange,
+    hint
+  } = props;
+
+  const inputWrapperRef = React.useRef();
+  const inputRef = React.useRef();
+
+  const createNewValue = React.useCallback(
+      (newValue) => {
+        return `${getFormatedValue(newValue)}${postfix}`;
+      },
+      [postfix]
+  );
+
+  const isValueInvalid = React.useCallback(
+      (value) => {
+        return value < minValue || value > maxValue;
+      },
+      [minValue, maxValue]
+  );
+
+  const handleNewValue = React.useCallback(
+      (newValue) => {
+        const digitValue = getCleanDigit(newValue);
+
+        if (isValueInvalid(digitValue)) {
+          if (!inputWrapperRef.current.classList.contains(CustomClass.WRAPPER_INVALID)) {
+            inputWrapperRef.current.classList.add(CustomClass.WRAPPER_INVALID);
+          }
+
+          if (digitValue < 0) {
+            return 0;
+          }
+        } else if (inputWrapperRef.current.classList.contains(CustomClass.WRAPPER_INVALID)) {
+          inputWrapperRef.current.classList.remove(CustomClass.WRAPPER_INVALID);
+        }
+
+        return newValue;
+      },
+      [isValueInvalid]
+  );
+
+  const handleIncrease = React.useCallback(
+      () => {
+        onCurrentValueChange((value) => {
+          value = getCleanDigit(value);
+          const newValue = value + stepValue;
+
+          return createNewValue(handleNewValue(newValue));
+        });
+      },
+      [onCurrentValueChange, stepValue, handleNewValue, createNewValue]
+  );
+
+  const handleDecrease = React.useCallback(
+      () => {
+        onCurrentValueChange((value) => {
+          value = getCleanDigit(value);
+          const newValue = value - stepValue;
+
+          return createNewValue(handleNewValue(newValue));
+        });
+      },
+      [onCurrentValueChange, stepValue, handleNewValue, createNewValue]
+  );
+
+  const handleInput = React.useCallback(
+      (evt) => {
+        onCurrentValueChange(handleNewValue(evt.target.value));
+      },
+      [onCurrentValueChange, handleNewValue]
+  );
+
+  const handleKeydown = React.useCallback(
+      (evt) => {
+        if (
+          !(REGEX_DIGITS.test(evt.key)
+          || evt.key === Key.SPACE
+          || evt.key === Key.BACKSPACE
+          || evt.key === Key.DELETE
+          || evt.key === Key.LEFT
+          || evt.key === Key.RIGHT
+          || evt.key === Key.TAB
+          || evt.key === Key.SHIFT)
+        ) {
+          evt.preventDefault();
+        }
+      },
+      []
+  );
+
+  const handleFocus = React.useCallback(
+      () => {
+        onCurrentValueChange((value) => {
+          return getFormatedValue(value);
+        });
+      },
+      [onCurrentValueChange]
+  );
+
+  const handleBlur = React.useCallback(
+      () => {
+        onCurrentValueChange((value) => {
+          return createNewValue(value);
+        });
+      },
+      [createNewValue, onCurrentValueChange]
+  );
+
+  return (
+    <React.Fragment>
+      {
+        labelText && (
+          <label className={labelClassName} htmlFor={inputId}>
+            {labelText}
+          </label>
+        )
+      }
+      <div
+        ref={inputWrapperRef}
+        className={CustomClass.WRAPPER}
+      >
+        {
+          controls && (
+            <button
+              className="calculator-params__input-button calculator-params__input-button--minus"
+              type="button"
+              onClick={handleDecrease}
+            >
+              Уменьшить
+            </button>
+          )
+        }
+        <p className="calculator-params__input-error">Некорректное значение</p>
+        <input
+          ref={inputRef}
+          className={inputClassName}
+          type="text"
+          value={currentValue}
+          min={minValue}
+          max={maxValue}
+          id={inputId}
+          onKeyDown={handleKeydown}
+          onInput={handleInput}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+        {
+          controls && (
+            <button
+              className="calculator-params__input-button calculator-params__input-button--plus"
+              onClick={handleIncrease}
+              type="button"
+            >
+              Увеличить
+            </button>
+          )
+        }
+      </div>
+      {
+        hint && (
+          <p className="calculator-params__input-hint">
+            От {getFormatedValue(minValue)} до {createNewValue(maxValue)}
+          </p>
+        )
+      }
+    </React.Fragment>
+  );
+};
+
+CalculatorInput.defaultProps = {
+  labelClassName: `calculator-params__label`,
+  labelText: ``,
+  inputClassName: `calculator-params__input`,
+  postfix: ``,
+  controls: false,
+  range: false,
+  movingRange: false,
+  hint: false,
+};
+
+CalculatorInput.propTypes = {
+  labelClassName: PropTypes.string,
+  labelText: PropTypes.string,
+  inputClassName: PropTypes.string,
+  inputId: PropTypes.string.isRequired,
+  minValue: PropTypes.number.isRequired,
+  maxValue: PropTypes.number.isRequired,
+  stepValue: PropTypes.number,
+  currentValue: PropTypes.string.isRequired,
+  onCurrentValueChange: PropTypes.func.isRequired,
+  postfix: PropTypes.string,
+  controls: PropTypes.bool,
+  range: PropTypes.bool,
+  movingRange: PropTypes.bool,
+  hint: PropTypes.bool,
+};
+
+export default CalculatorInput;
--- src/scripts/components/calculator-params/calculator-params.jsx
+++ src/scripts/components/calculator-params/calculator-params.jsx
@@ -1,198 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {Key} from "../../constants";
+import CalculatorInput from "../calculator-input/calculator-input";
 
-const REGEX_DIGITS = /^\d+$/;
-const START_VALUE = 2_000_000;
-const VALUE_STEP = 100_000;
-const POSTFIX = ` рублей`;
-
-const WRAPPER_CLASS = `calculator-params__input-wrapper`;
-
-const CustomClass = {
-  WRAPPER: WRAPPER_CLASS,
-  WRAPPER_INVALID: `${WRAPPER_CLASS}--invalid`,
-};
-
-const Value = {
+const CostLimit = {
   MIN: 1_200_000,
   MAX: 25_000_000,
 };
 
-const getCleanDigit = (dirtyValue) => {
-  if (typeof dirtyValue === `number`) {
-    return dirtyValue;
-  }
+// const DurationLimit = {
+//   MIN: 5,
+//   MAX: 30,
+// };
 
-  const cleanDigit = parseInt(
-      String(dirtyValue).split(``).filter((char) => REGEX_DIGITS.test(char)).join(``),
-      10
-  );
-
-  if (isNaN(cleanDigit)) {
-    return 0;
-  }
-
-  return cleanDigit;
+const Postfix = {
+  COST: ` рублей`,
+  PAYMENT: ` рублей`,
+  DURATION: ` лет`,
 };
 
-const getFormatedValue = (value) => {
-  if (typeof value === `string`) {
-    value = getCleanDigit(value);
-  }
-
-  return value.toLocaleString();
-};
-
-const createNewValue = (newValue) => {
-  return `${getFormatedValue(newValue)}${POSTFIX}`;
-};
-
-const isValueInvalid = (value) => {
-  return value < Value.MIN || value > Value.MAX;
+const Step = {
+  COST: 100_000,
+  PAYMENT: 5,
+  DURATION: 1,
 };
 
 const CalculatorParams = (props) => {
   const {className = ``} = props;
 
-  const inputWrapperRef = React.useRef();
-  const inputRef = React.useRef();
-
-  const [currentValue, setCurrentValue] = React.useState(createNewValue(START_VALUE));
-
-  const handleNewValue = React.useCallback(
-      (newValue) => {
-        const digitValue = getCleanDigit(newValue);
-
-        if (isValueInvalid(digitValue)) {
-          if (!inputWrapperRef.current.classList.contains(CustomClass.WRAPPER_INVALID)) {
-            inputWrapperRef.current.classList.add(CustomClass.WRAPPER_INVALID);
-          }
-
-          if (digitValue < 0) {
-            return 0;
-          }
-        } else if (inputWrapperRef.current.classList.contains(CustomClass.WRAPPER_INVALID)) {
-          inputWrapperRef.current.classList.remove(CustomClass.WRAPPER_INVALID);
-        }
-
-        return newValue;
-      },
-      []
-  );
-
-  const handleIncrease = React.useCallback(
-      () => {
-        setCurrentValue((value) => {
-          value = getCleanDigit(value);
-          const newValue = value + VALUE_STEP;
-
-          return createNewValue(handleNewValue(newValue));
-        });
-      },
-      [handleNewValue]
-  );
-
-  const handleDecrease = React.useCallback(
-      () => {
-        setCurrentValue((value) => {
-          value = getCleanDigit(value);
-          const newValue = value - VALUE_STEP;
-
-          return createNewValue(handleNewValue(newValue));
-        });
-      },
-      [handleNewValue]
-  );
-
-  const handleInput = React.useCallback(
-      (evt) => {
-        setCurrentValue(handleNewValue(evt.target.value));
-      },
-      [handleNewValue]
-  );
-
-  const handleKeydown = React.useCallback(
-      (evt) => {
-        if (
-          !(REGEX_DIGITS.test(evt.key)
-          || evt.key === Key.SPACE
-          || evt.key === Key.BACKSPACE
-          || evt.key === Key.DELETE
-          || evt.key === Key.LEFT
-          || evt.key === Key.RIGHT
-          || evt.key === Key.TAB
-          || evt.key === Key.SHIFT)
-        ) {
-          evt.preventDefault();
-          // console.log(`none`);
-        }
-      },
-      []
-  );
-
-  const handleFocus = React.useCallback(
-      () => {
-        setCurrentValue((value) => {
-          return getFormatedValue(value);
-        });
-      },
-      []
-  );
-
-  const handleBlur = React.useCallback(
-      () => {
-        setCurrentValue((value) => {
-          return createNewValue(value);
-        });
-      },
-      []
-  );
+  const [currentCost, setCurrentCost] = React.useState(`2 000 000${Postfix.COST}`);
 
   return (
     <div className={`${className} calculator-params`}>
       <h3 className="calculator-params__title">
         Шаг 2. Введите параметры кредита
       </h3>
-      <label className="calculator-params__label" htmlFor="estate-cost">
-        Стоимость недвижимости
-      </label>
-      <div
-        ref={inputWrapperRef}
-        className={CustomClass.WRAPPER}
-      >
-        <button
-          className="calculator-params__input-button calculator-params__input-button--minus"
-          type="button"
-          onClick={handleDecrease}
-        >
-          Уменьшить стоимость
-        </button>
-        <p className="calculator-params__input-error">Некорректное значение</p>
-        <input
-          ref={inputRef}
-          className="calculator-params__input"
-          type="text"
-          value={currentValue}
-          min={Value.MIN}
-          max={Value.MAX}
-          id="estate-cost"
-          onKeyDown={handleKeydown}
-          onInput={handleInput}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-        />
-        <button
-          className="calculator-params__input-button calculator-params__input-button--plus"
-          onClick={handleIncrease}
-          type="button"
-        >
-          Увеличить стоимость
-        </button>
-      </div>
-      <p className="calculator-params__input-hint">
-        От 1 200 000  до 25 000 000 рублей
-      </p>
+      <CalculatorInput
+        labelText="Стоимость недвижимости"
+        inputId="input-cost"
+        minValue={CostLimit.MIN}
+        maxValue={CostLimit.MAX}
+        stepValue={Step.COST}
+        currentValue={currentCost}
+        onCurrentValueChange={setCurrentCost}
+        postfix={Postfix.COST}
+        controls={true}
+        hint={true}
+      />
     </div>
   );
 };
